aed39583d0e3d9feccad2870ab5da862e6c755e8
Andrew Haarsager <athaarsager@gmail.com>
2024-01-26 16:44:10 -0600

    Client PUT route created. Updated return of GalleryItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,28 @@
-function GalleryItem() {
-    return(
+import { useState } from React;
+import axios from "axios";
+
+function GalleryItem( {getPics, pic, key} ) {
+
+    const [isClicked, setIsClicked] = useState(false);
+ 
+    const toggleDisplay = () => setIsClicked(!isClicked)
+
+    const updateLikes = (picId) => {
+        const picId = pic.id
+        axios.put(`/api/gallery/like/${picId}`)
+        .then(() => {
+            getPics();
+        })
+        .catch((error) => {
+            console.error("Error in client PUT:", error);
+        });
+    }
+
+    return (
+
         <>
+        <li onClick={toggleDisplay}>{pic}</li>
+        <button onClick={updateLikes}>Love it!</button>
         </>
     );
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,31 +1,32 @@
 import axios from "axios";
-import React from "react";
 import { useState } from React;
 import GalleryItem from "../GalleryItem/GalleryItem";
 
 function GalleryList() {
 
     const [picList, setPicList] = useState([]);
-
+    //may need useEffect
     const getPics = () => {
         axios.get("/api/gallery")
-        .then((response) => {
-            setPicList(response.data);
-            console.log(picList);
-        })
-        .catch((error) => {
-            console.error("Error in client GET:", error);
-        });
+            .then((response) => {
+                setPicList(response.data);
+                console.log(picList);
+            })
+            .catch((error) => {
+                console.error("Error in client GET:", error);
+            });
     }
 
-    return(
+    return (
         <>
-        <ul>
-            {picList.map(pic => (
-                <GalleryItem
-                getPics= { getPics }/>
-            ))}
-        </ul>
+            <ul>
+                {picList.map((pic) => (
+                    <GalleryItem
+                        getPics={getPics}
+                        pic={pic}
+                    />
+                ))}
+            </ul>
         </>
     );
 }
