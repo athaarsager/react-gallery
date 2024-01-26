61c72c70851d6101c71edbc0bee4ba43613ece12
Andrew Haarsager <athaarsager@gmail.com>
2024-01-26 15:00:40 -0600

    Server Put request created. Fixed typos in Get request

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,18 +5,30 @@ const pool = require("../modules/pool");
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
+  const picId = req.params.id;
+  const queryText = `UPDATE "gallery" SET "likes" = "likes" + 1 WHERE "id" = $1;`;
+  pool.query(queryText, [picId])
+  .then((response) => {
+    res.sendStatus(200);
+  })
+  .catch((error) => {
+    console.error("Error in server PUT:", error);
+    res.sendStatus(500);
+  });
+
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
-  const queryText = `SELECT * FROM "gallery"`;
+  const queryText = `SELECT * FROM "gallery;"`;
   pool.query(queryText)
   .then((response) => {
     res.send(response.rows);
   })
   .catch((error) => {
     console.error("Error in server GET:", error);
+    res.sendStatus(500);
   });
   
 });
