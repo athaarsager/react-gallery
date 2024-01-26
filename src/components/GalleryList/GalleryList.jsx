b4442acbdab9a0592866010f259ad56e0aeb0bf3
Andrew Haarsager <athaarsager@gmail.com>
2024-01-26 15:07:22 -0600

    Created boilerplate for GalleryList

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,15 @@
+import Axios from "axios";
+import React from "react";
+import { useState } from React;
+
+function GalleryList() {
+    return(
+        <>
+        <ul>
+
+        </ul>
+        </>
+    );
+}
+
+export default GalleryList;
