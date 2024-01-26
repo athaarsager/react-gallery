659dce2b6b978ea475b71272da1a1b7463127b6e
Andrew Haarsager <athaarsager@gmail.com>
2024-01-26 14:26:21 -0600

    Added photos to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,12 @@ function App() {
 
         <p>The gallery goes here!</p>
         <img src="images/goat_small.jpg"/>
+        <img src="images/fira_caldera.jpeg"/>
+        <img src="images/great_library_in_ephesus.jpeg"/>
+        <img src="images/marimba.jpeg"/>
+        <img src="images/graduation.jpeg"/>
+        <img src="images/cool_drumming_photo.jpeg"/>
+        <img src="images/the_band.jpeg"/>
       </div>
     );
 }
