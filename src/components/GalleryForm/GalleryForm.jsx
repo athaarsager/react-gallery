51a9f157dd6c6bbd3d059f35250adf4a52643680
Andrew Haarsager <athaarsager@gmail.com>
2024-01-27 15:26:38 -0600

    Added first state variable. Added form fields for extra information

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
+import axios from "axios";
 import { Form, Button, Row, Col } from "react-bootstrap";
 function GalleryForm() {
+
+    const [picUrl, setPicUrl] = useState("");
+    const addNewPic = (e) => {
+    }
+
     return (
         <div>
             <Form>
                 <Row>
                     <Col md={{ span: 3, offset: 1}}>
+                        <h4>Add a New Picture Here!</h4>
                         <Form.Group controlId="formImageUrl">
-                            <Form.Label><strong>Add a New Picture Here!</strong></Form.Label>
-                            <Form.Control type="url" placeholder="https://i.imgur.com/J5re04j.png" />
+                            <Form.Label className="my-1">Pic Url:</Form.Label>
+                            <Form.Control type="url" placeholder="https://i.imgur.com/J5re04j.png" value={picUrl} />
+                        </Form.Group>
+                        <Form.Group controlId="picTitle">
+                            <Form.Label className="my-1">Title of Pic:</Form.Label>
+                            <Form.Control type="text" placeholder="Awesome Title"/>
+                        </Form.Group>
+                        <Form.Group controlId="picDescription">
+                            <Form.Label className="my-1">Description of Pic:</Form.Label>
+                            <Form.Control type="text" placeholder="Fun Description"/>
                         </Form.Group>
                         <Button variant="secondary" type="Submit" className="my-2">Submit</Button>
                     </Col>
