7873a61d7995228fe8b51f6148a53361ba2fb9ac
Andrew Haarsager <athaarsager@gmail.com>
2024-01-27 16:08:36 -0600

    Client Post Request Works

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -8,6 +8,7 @@ function GalleryForm({getPics}) {
     const [picDescription, setPicDescription] = useState("");
 
     const addNewPic = (e) => {
+        e.preventDefault();
         axios.post("/api/gallery", {url: picUrl, title: picTitle, description: picDescription})
         .then(() => {
             getPics();
@@ -19,21 +20,21 @@ function GalleryForm({getPics}) {
 
     return (
         <div>
-            <Form>
+            <Form onSubmit={addNewPic}>
                 <Row>
                     <Col md={{ span: 3, offset: 1}}>
                         <h4>Add a New Picture Here!</h4>
                         <Form.Group controlId="formImageUrl">
                             <Form.Label className="my-1">Pic Url:</Form.Label>
-                            <Form.Control type="url" placeholder="https://i.imgur.com/J5re04j.png" value={picUrl} onChange={setPicUrl} required/>
+                            <Form.Control type="url" placeholder="https://i.imgur.com/J5re04j.png" value={picUrl} onChange={(e) => setPicUrl(e.target.value)} required/>
                         </Form.Group>
                         <Form.Group controlId="picTitle">
                             <Form.Label className="my-1">Title of Pic:</Form.Label>
-                            <Form.Control type="text" placeholder="Awesome Title" value={picTitle} onChange={setPicTitle} required/>
+                            <Form.Control type="text" placeholder="Awesome Title" value={picTitle} onChange={(e) => setPicTitle(e.target.value)} required/>
                         </Form.Group>
                         <Form.Group controlId="picDescription">
                             <Form.Label className="my-1">Description of Pic:</Form.Label>
-                            <Form.Control type="text" placeholder="Fun Description" value={picDescription} onChange={setPicDescription} required/>
+                            <Form.Control type="text" placeholder="Fun Description" value={picDescription} onChange={(e) => setPicDescription(e.target.value)} required/>
                         </Form.Group>
                         <Button variant="secondary" type="Submit" className="my-2">Submit</Button>
                     </Col>
