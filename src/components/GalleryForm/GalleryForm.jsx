0819c393f06248d8f666c6c312713a1d0d61bb19
Andrew Haarsager <athaarsager@gmail.com>
2024-01-27 15:05:39 -0600

    Adjusted form and button styling

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 function GalleryForm() {
     return (
         <div>
-            <form>
-                
-            </form>
+            <Form>
+                <Row>
+                    <Col md={{ span: 3, offset: 1}}>
+                        <Form.Group controlId="formImageUrl">
+                            <Form.Label><strong>Add a New Picture Here!</strong></Form.Label>
+                            <Form.Control type="url" placeholder="https://i.imgur.com/J5re04j.png" />
+                        </Form.Group>
+                        <Button variant="secondary" type="Submit" className="my-2">Submit</Button>
+                    </Col>
+                </Row>
+            </Form>
         </div>
     );
 }
