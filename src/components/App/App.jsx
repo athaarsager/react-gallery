14e500c6669644e107ec011eaf12692de412abe8
Andrew Haarsager <athaarsager@gmail.com>
2024-01-29 16:59:17 -0600

    Added deployment code. Began adding Hashrouter stuff to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
 import GalleryForm from "../GalleryForm/GalleryForm";
 import GalleryList from "../GalleryList/GalleryList";
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 
 function App() {
 
@@ -11,30 +12,39 @@ function App() {
 
   const getPics = () => {
     axios.get("/api/gallery")
-        .then((response) => {
-            setPicList(response.data);
-        })
-        .catch((error) => {
-            console.error("Error in client GET:", error);
-        });
-}
-    return (
+      .then((response) => {
+        setPicList(response.data);
+      })
+      .catch((error) => {
+        console.error("Error in client GET:", error);
+      });
+  }
+  return (
+    <Router>
       <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
+          <nav>
+            <ul>
+              <li><NavLink>Gallery</NavLink></li>
+              <li><NavLink>Form</NavLink></li>
+              <li><NavLink>About</NavLink></li>
+            </ul>
+          </nav>
         </header>
         <p id="info-text"><em>Click an image to see its description and vice versa!</em></p>
         <main>
-        <GalleryForm
-        getPics={getPics}
-        />
-        <GalleryList
-        getPics={getPics}
-        picList={picList}
-        />
+          <GalleryForm
+            getPics={getPics}
+          />
+          <GalleryList
+            getPics={getPics}
+            picList={picList}
+          />
         </main>
       </div>
-    );
+    </Router>
+  );
 }
 
 export default App;
